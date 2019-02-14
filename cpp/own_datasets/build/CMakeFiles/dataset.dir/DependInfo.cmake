2d9335069daf410fec701e7d1d713309a285666b
masanari kimura <noconoco.lib@gmail.com>
2019-02-14 15:22:20 +0900

    add cpp own dataset example

---
+++ cpp/own_datasets/build/CMakeFiles/dataset.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/kimura/workspace/examples/cpp/own_datasets/dataset.cpp" "/Users/kimura/workspace/examples/cpp/own_datasets/build/CMakeFiles/dataset.dir/dataset.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Users/kimura/lib/libtorch/include"
+  "/Users/kimura/lib/libtorch/include/torch/csrc/api/include"
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cpp/own_datasets/build/CMakeFiles/dataset.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dataset.dir/dataset.cpp.o"
+  "dataset.pdb"
+  "dataset"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dataset.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
